82fd9d224a54e0d8cdeb801ac8ee9aed47665aec
Jinuk You <nugisquare@gmail.com>
2016-06-30 22:34:04 +0900

    Update no1057.c

--- koistudy/no1057.c
+++ koistudy/no1057.c
@@ -9,26 +9,12 @@ int main(void)
 	{
 		scanf("%d", &tmp);
 		if(tmp >= k && answer == 0) answer = i;
+//		another solution
+// 		if(answer != 0) break;
+// 		scanf("%d", &tmp);
+// 		if(tmp >= k) answer = i;
 	}
 	if(answer != 0) printf("%d", answer);
 	else printf("%d", n+1);
 	return 0;
 }
-
-// #include <stdio.h>
-
-// int main(void)
-// {
-// 	int i, n, k, tmp, answer;
-// 	answer = 0;
-// 	scanf("%d %d", &n, &k);
-// 	for(i=1; i<=n; i++)
-// 	{
-// 		if(answer != 0) break;
-// 		scanf("%d", &tmp);
-// 		if(tmp >= k) answer = i;
-// 	}
-// 	if(answer != 0) printf("%d", answer);
-// 	else printf("%d", n+1);
-// 	return 0;
-// }
